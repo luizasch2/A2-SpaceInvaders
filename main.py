5ceff3871828403df641634df09946af9ba87c2d
Luana Hatab <99187348+luanahatab@users.noreply.github.com>
2022-12-01 17:37:23 +0000

    player conf main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from settings import *
-
+from player_config import PlayerConfig
 
 #init pygame
 pygame.init()
 
 background = scene_config()
+player = PlayerConfig()
 
 while True:
     screen.blit(background, (0, 0))
