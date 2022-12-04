ca79b69b3bc8731efc64f5641b14a0f34c29b068
MarianaLima2000 <marianalimab.2000@gmail.com>
2022-12-04 08:50:55 -0300

    adiciona classe referente aos inimigos do jogo

---
+++ enemy.py
@@ -0,0 +1,36 @@
+import pygame
+from settings import *
+import random
+
+class Enemy():
+    def __init__(self, x_position, y_position, skin_src: str = './img/002-ghost.png', skin_hard_src: str =  './img/003-ghost.png',
+                vel: float = 0.5, state: str = 'ready'):
+        ## iniciais definidos:
+        ## enemySkin = pygame.image.load('./img/002-ghost.png')
+        ## enemySkin2 = pygame.image.load('./img/003-ghost.png')
+        self.image = pygame.image.load(skin_src)
+        self.image_hard = pygame.image.load(skin_hard_src)
+        self.X = x_position
+        self.Y = y_position
+        self.state = state
+        self.vel = vel
+        
+    def change_skin(self, skin_src: str):
+        self.image = pygame.image.load(skin_src)
+    
+    def change_skin_hard(self, skin_src: str):
+        self.image_hard = pygame.image.load(skin_src)
+    
+    def change_to_skin_hard(self):
+        self.image = self.image_hard
+    
+    def change_position(self, new_x, new_y):
+        self.X = new_x
+        self.Y = new_y
+    
+    def change_vel(self, new_vel):
+        self.vel = new_vel
+    
+    def blit(self, x, y):
+        screen.blit(self.image, (x, y))
+    
