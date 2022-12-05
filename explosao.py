42cf4987f3522809a026bc5b58994ac94dc1caac
Luiza Schneider Sampaio <98579033+luizasch2@users.noreply.github.com>
2022-12-05 05:48:53 -0300

    sprite correta

--- explosao.py
+++ explosao.py
@@ -6,62 +6,30 @@ class Explosao(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
-        self.sprites.append(pygame.image.load('./sprites/Explosion_01.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_02.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_03.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_04.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_05.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_06.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_07.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_08.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_09.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_10.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_11.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_12.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_13.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_14.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_15.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_16.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_17.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_18.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_19.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_20.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_21.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_22.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_23.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_24.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_25.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_26.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_27.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_28.png'))
-        self.sprites.append(pygame.image.load('./sprites/Explosion_29.png'))
+        for i in range(1,30):
+            self.sprites.append(pygame.image.load(f'./sprites/Explosion_{i}.png'))
     
-        self.atual = 0
-        self.image = self.sprites[self.atual]
-        self.animar = False
-
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 100, 100
-    # def posicao_sprite(self, x, y):
-        
-
-    def explodir(self):
-        self.animar = True
+        self.index = 0
+        self.image = self.sprites[self.index]
+        self.image = pygame.transform.scale(self.image, (100*4, 100*4))
+        self.counter = 0
 
     def update(self):
-        if self.animar == True:
-            self.atual += 0.05
-            if self.atual >= len(self.sprites):
-                self.atual = 0
-                self.animar = False
-            self.image = self.sprites[int(self.atual)]
-            self.image = pygame.transform.scale(self.image, (100*3, 100*3))
-
-
-
-
-
+        self.counter += 0.05
+        if self.counter >= 10:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.sprites):
+                self.kill()
+            else:
+                self.image = self.sprites[self.index]
+                self.image = pygame.transform.scale(self.image, (100*4, 100*4))
     
     
+    def posicao(self, x, y):      
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
 
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
